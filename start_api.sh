b7a98289e17da5331e3402098d954bb06398132d
Dmitry Vesenniy <press8328@protonmail.com>
2021-10-24 13:58:44 +0700

    fix: Добавить upgrade pip в файл установки. Добавить ip и порт в start_api.sh

--- start_api.sh
+++ start_api.sh
@@ -2,5 +2,5 @@
 
 source ./venv/bin/activate &&
   cd app/api &&
-  python -m gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+  python -m gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app -b "0.0.0.0:8000"
 # python ./app/api/main.py
